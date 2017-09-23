4100a8c7b39a16e208ede5030d2bd4bad0250705
RohanVanDerMerwe <u14005337@tuks.co.za>
2017-09-23 17:40:05 +0200

    textbox add

--- PETSystem/PETSystem/Create Course.Designer.cs
+++ PETSystem/PETSystem/Create Course.Designer.cs
@@ -36,13 +36,16 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtCourseDuration = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(335, 172);
+            this.btnCancel.Location = new System.Drawing.Point(257, 306);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(146, 23);
+            this.btnCancel.Size = new System.Drawing.Size(195, 28);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -50,9 +53,10 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(183, 172);
+            this.btnSave.Location = new System.Drawing.Point(42, 297);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(146, 23);
+            this.btnSave.Size = new System.Drawing.Size(195, 28);
             this.btnSave.TabIndex = 4;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -60,60 +64,84 @@
             // 
             // txtCourseCost
             // 
-            this.txtCourseCost.Location = new System.Drawing.Point(145, 78);
+            this.txtCourseCost.Location = new System.Drawing.Point(193, 96);
+            this.txtCourseCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCourseCost.Name = "txtCourseCost";
-            this.txtCourseCost.Size = new System.Drawing.Size(337, 20);
+            this.txtCourseCost.Size = new System.Drawing.Size(356, 22);
             this.txtCourseCost.TabIndex = 2;
             this.txtCourseCost.TextChanged += new System.EventHandler(this.txtCourseCost_TextChanged);
             // 
             // txtCourseName
             // 
-            this.txtCourseName.Location = new System.Drawing.Point(145, 42);
+            this.txtCourseName.Location = new System.Drawing.Point(193, 52);
+            this.txtCourseName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCourseName.Name = "txtCourseName";
-            this.txtCourseName.Size = new System.Drawing.Size(337, 20);
+            this.txtCourseName.Size = new System.Drawing.Size(448, 22);
             this.txtCourseName.TabIndex = 1;
             this.txtCourseName.TextChanged += new System.EventHandler(this.txtCourseName_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(29, 119);
+            this.label5.Location = new System.Drawing.Point(39, 146);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.Size = new System.Drawing.Size(115, 17);
             this.label5.TabIndex = 38;
             this.label5.Text = "Course Duration:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 45);
+            this.label3.Location = new System.Drawing.Point(39, 55);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.Size = new System.Drawing.Size(98, 17);
             this.label3.TabIndex = 37;
             this.label3.Text = "Course Name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 81);
+            this.label2.Location = new System.Drawing.Point(39, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.Size = new System.Drawing.Size(89, 17);
             this.label2.TabIndex = 36;
             this.label2.Text = "Course Cost:";
             // 
             // txtCourseDuration
             // 
-            this.txtCourseDuration.Location = new System.Drawing.Point(145, 116);
+            this.txtCourseDuration.Location = new System.Drawing.Point(193, 143);
+            this.txtCourseDuration.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCourseDuration.Name = "txtCourseDuration";
-            this.txtCourseDuration.Size = new System.Drawing.Size(337, 20);
+            this.txtCourseDuration.Size = new System.Drawing.Size(259, 22);
             this.txtCourseDuration.TabIndex = 3;
             this.txtCourseDuration.TextChanged += new System.EventHandler(this.txtCourseDuration_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Royalty Amount";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 22);
+            this.textBox1.TabIndex = 40;
+            // 
             // Create_Course
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 232);
+            this.ClientSize = new System.Drawing.Size(691, 358);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCourseDuration);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
@@ -122,6 +150,7 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Create_Course";
             this.Text = "Create_Course";
             this.ResumeLayout(false);
@@ -139,5 +168,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtCourseDuration;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
