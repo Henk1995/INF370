1d61b7c33214a5c82f9d336ff2b3b0919df531a0
jwm23s <31405038+jwm23s@users.noreply.github.com>
2017-10-04 14:17:16 +0200

    commit om te kan download

--- PETSystem/PETSystem/ConnectString.cs
+++ PETSystem/PETSystem/ConnectString.cs
@@ -5,82 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using PETSystem;
+using System.IO;
 
 namespace PETSystem
 {
     class ConnectString
     {
-        // Getting Started Documentation
-
-        /*   
-             *Download die database vanaf google drive
-                 - File path:
-                  drive -> INF 370 -> del5 -> "inf370Reg.bacpac" 
-             *Import die database in Microsoft SQL Server Management Studio ( SSMS vir kort ;D )
-                 - Maak SSMS oop en regs click op databases
-                 - Select Import Data-Tier Application..
-                 - Select "NEXT"
-                 - Select "Browse"
-                 - FILE PATH BEHOORT TE WEES:
-                    Downloads -> "inf370Reg.bacpac" 
-                    ( Wees rustig hy dink )
-                 - So iets sal wys =  "C:\Users\%JOU USER NAME%\Downloads\inf370Reg.bacpac" ( Pretty cool )
-                 - Select "NEXT"
-                 - "New Databse Name" = inf370Reg ( Los hom so )
-                 - Select "NEXT"
-                    ( Gan maak koffie... Dit kan i rukkie vat... )
-                - As alles goed gegaan het sal jy kan databases in die "Object Explorer" ( LINKS IN SMSS ) kan refresh en die Database, "inf370Reg", sal daar wees.
-
-        */
-
-
-        /* HOE OM JOU CONNECTION STRING TE KRY
-
-            *KRY LINQ OM TE WERK
-                - Double-click PET_DB.dbml
-                - In server explorer druk op Connect to database knoppie ( net bo die woord "AZURE" )
-                - i Window maak oop. In "server name" dropdown kies jou SSMS server naam ( hy mag dalk i rukkie vries so gee hom kans )
-                - in dropdown "Select or enter database name" select die imported database van die boonste stappe ( inf370Reg )
-                - Links onder click "TEST CONNECTION"
-                    - Hy behoort te se "connection successfull"
-                - Regs onder click "ADVANCED"
-                    - Heel onder is daar i textbox met klomp geskryf in. iets soos "Data Source=JOHN-MAC-WIN;Initial Catalog=inf370Reg;Integrated Security=True"
-                    - Highlight en copy al daai, Click "OK"
-                    - Click weer "OK"
-                - Onder Data Connections in die Server Explorer sal die nuwe connection wys
-
-            * KRY DIE SQL OM TE WERK
-                - Gaan hier onder na "Almal se connection strings" en kry jou naam.
-                - Paste die goed wat jy hier bo gecopy het in die "" aan die einde van die Public static string DBC in en uncomment ie lyntjie
-                - Maak net seker dat al die ander mense se connection strings uitgecomment is.
-                - Run die program en kyk of hy werk.
-                 
-        */
-
-        // ALMAL SE CONNECTION STRINGS
-
-        //Jan se connection string
-     public static string DBC = "Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\370.mdf;Integrated Security=True;";
-
-        //John se connection string
-//        public static string DBC = "Data Source=JOHN-MAC-WIN;Initial Catalog=exportFotos;Integrated Security=True";
-
-        //Henk se connection string    + Copy jou goed hier in en uncomment die lyn hier onder
-       // public static string DBC = "Data Source=.;Initial Catalog=RefIntegrityExport;Integrated Security=True";
-
-
-        //Sam se connection string    + Copy jou goed hier in en uncomment die lyn hier onder
-        //public static string DBC = "Data Source=.;Initial Catalog=inf370Reg;Integrated Security=True";
-
-        //Rohan se connection string    + Copy jou goed hier in en uncomment die lyn hier onder
-        //public static string DBC = "Data Source=.;Initial Catalog=inf370Reg;Integrated Security=True";
-
-
-        // LOS HIERDIE NET SO
-        public static SqlConnection connectstring = new SqlConnection(DBC);
-        //HEnk se comment
-        //het goed geskuif vir test commit
-        public string VIEW;
+      
         // Henk se global int
         public static int ID;
         public static int CourseID;
@@ -131,6 +62,78 @@ namespace PETSystem
         public static bool firstLogin;
         //Training Course Result
         public static int TrainingCourseIDForResult;
+        // Getting Started Documentation
+
+        /*   
+             *Download die database vanaf google drive
+                 - File path:
+                  drive -> INF 370 -> del5 -> "inf370Reg.bacpac" 
+             *Import die database in Microsoft SQL Server Management Studio ( SSMS vir kort ;D )
+                 - Maak SSMS oop en regs click op databases
+                 - Select Import Data-Tier Application..
+                 - Select "NEXT"
+                 - Select "Browse"
+                 - FILE PATH BEHOORT TE WEES:
+                    Downloads -> "inf370Reg.bacpac" 
+                    ( Wees rustig hy dink )
+                 - So iets sal wys =  "C:\Users\%JOU USER NAME%\Downloads\inf370Reg.bacpac" ( Pretty cool )
+                 - Select "NEXT"
+                 - "New Databse Name" = inf370Reg ( Los hom so )
+                 - Select "NEXT"
+                    ( Gan maak koffie... Dit kan i rukkie vat... )
+                - As alles goed gegaan het sal jy kan databases in die "Object Explorer" ( LINKS IN SMSS ) kan refresh en die Database, "inf370Reg", sal daar wees.
+
+        */
+
+
+        /* HOE OM JOU CONNECTION STRING TE KRY
+
+            *KRY LINQ OM TE WERK
+                - Double-click PET_DB.dbml
+                - In server explorer druk op Connect to database knoppie ( net bo die woord "AZURE" )
+                - i Window maak oop. In "server name" dropdown kies jou SSMS server naam ( hy mag dalk i rukkie vries so gee hom kans )
+                - in dropdown "Select or enter database name" select die imported database van die boonste stappe ( inf370Reg )
+                - Links onder click "TEST CONNECTION"
+                    - Hy behoort te se "connection successfull"
+                - Regs onder click "ADVANCED"
+                    - Heel onder is daar i textbox met klomp geskryf in. iets soos "Data Source=JOHN-MAC-WIN;Initial Catalog=inf370Reg;Integrated Security=True"
+                    - Highlight en copy al daai, Click "OK"
+                    - Click weer "OK"
+                - Onder Data Connections in die Server Explorer sal die nuwe connection wys
+
+            * KRY DIE SQL OM TE WERK
+                - Gaan hier onder na "Almal se connection strings" en kry jou naam.
+                - Paste die goed wat jy hier bo gecopy het in die "" aan die einde van die Public static string DBC in en uncomment ie lyntjie
+                - Maak net seker dat al die ander mense se connection strings uitgecomment is.
+                - Run die program en kyk of hy werk.
+                 
+        */
+        //Data Source =.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\FinaalINF370.mdf;Integrated Security = True;
+        // ALMAL SE CONNECTION STRINGS
+
+        //Jan se connection string
+       
+        public static string DBC = " Data Source =.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\FinaalINF370.mdf;Integrated Security = True;";
+       
+        //John se connection string
+        //        public static string DBC = "Data Source=JOHN-MAC-WIN;Initial Catalog=exportFotos;Integrated Security=True";
+
+        //Henk se connection string    + Copy jou goed hier in en uncomment die lyn hier onder
+        // public static string DBC = "Data Source=.;Initial Catalog=RefIntegrityExport;Integrated Security=True";
+
+
+        //Sam se connection string    + Copy jou goed hier in en uncomment die lyn hier onder
+        //public static string DBC = "Data Source=.;Initial Catalog=inf370Reg;Integrated Security=True";
+
+        //Rohan se connection string    + Copy jou goed hier in en uncomment die lyn hier onder
+        //public static string DBC = "Data Source=.;Initial Catalog=inf370Reg;Integrated Security=True";
+
+
+        // LOS HIERDIE NET SO
+        public static SqlConnection connectstring = new SqlConnection(DBC);
+        //HEnk se comment
+        //het goed geskuif vir test commit
+        
         
         
     }
